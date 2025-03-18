dad6686ef6336f1e1066ddeadab7ab2e43169fd6
Pranjalmohite21 <pranjalmohitevk@gmail.com>
2025-03-18 10:13:56 +0530

    Add files via upload

---
+++ SortNumbers.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+ class SortNumbers {
+public static void main(String[]args){
+int[] numbers={42,12,89,5,33,76,23,54,90,17};
+Arrays.sort(numbers);
+System.out.println("sorted numbers in ascending order:"+Arrays.toString(numbers));
+}
+}
